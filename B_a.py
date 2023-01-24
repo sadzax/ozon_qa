b1532b6acbb957c3d30c9f876062eadf632f79e9
Marat Ramazanov <sadzax@gmail.com>
2023-01-24 10:54:07 +0300

    Solving +

--- B_a.py
+++ B_a.py
@@ -1,15 +1,11 @@
 t = int(input())
 for i in range(t):
     n = int(input())
-    p = (input().split())
-    p_dict = {}
-    p_dict_discount = {}
+    p = input().split()
+    p_set = list(set(p))
     total = 0
-    for el in p:
-        p[p.index(el)] = int(el)
-        p_dict[el] = p.count(int(el))
-    for key in p_dict:
-        discount = p_dict[key] // 3
-        p_dict_discount[key] = p_dict[key]-int(discount)
-        total = total + int(key) * p_dict_discount[key]
+    for el in p_set:
+        count = p.count(el)
+        discount = count // 3
+        total = total + int(el) * (count - count // 3)
     print(total)
