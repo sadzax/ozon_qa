f13c30bb6c46e2a3bab5b4048bd41afe09747c8c
Marat Ramazanov <sadzax@gmail.com>
2023-01-25 18:37:06 +0300

    Solving

--- B_a.py
+++ B_a.py
@@ -1,11 +1,6 @@
 t = int(input())
 for i in range(t):
     n = int(input())
-    p = input().split()
+    p = list(map(int, input().split()))
     p_set = list(set(p))
-    total = 0
-    for el in p_set:
-        count = p.count(el)
-        discount = count // 3
-        total = total + int(el) * (count - count // 3)
-    print(total)
+    print(sum(map(lambda x: x * (p.count(x) - p.count(x) // 3), p_set)))
