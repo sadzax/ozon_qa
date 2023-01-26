99e776749fe9003cd8c326a27f81ac313464f7e0
Marat Ramazanov <sadzax@gmail.com>
2023-01-26 18:41:46 +0300

    Solving

--- D_a.py
+++ D_a.py
@@ -1,8 +1,8 @@
 t = int(input())
 n = int(input())  # rows
 m = int(input())  # columns
-# b = [[3, 4, 1], [2, 2, 5], [2, 4, 2], [2, 2, 1]]
-# arr = b.copy()
+b = [[3, 4, 1], [2, 2, 5], [2, 4, 2], [2, 2, 1]]
+arr = b.copy()
 arr = []
 for _ in range(n):
     row = list(map(int, input().split()))
@@ -16,13 +16,14 @@ def quicksort(array, x):
         return array
     else:
         pivot = array[0][x-1]
-        less = [[ls for ls in array[1:] if ls[x-1] < pivot]]
-        greater = [[ls for ls in array[1:] if ls[x-1] >= pivot]]
-        return quicksort(less, x) + [array[0]] + quicksort(greater, x)
+        less = [ls for ls in array[1:] if ls[x-1] < pivot]
+        greater = [ls for ls in array[1:] if ls[x-1] >= pivot]
+        return quicksort(less, x) + array[0] + quicksort(greater, x)
 
 
-new_arr = quicksort(arr, 2)
+na = quicksort(arr, 3)
 
 
 for i in kc:
     new_arr = quicksort(arr, i)
+
