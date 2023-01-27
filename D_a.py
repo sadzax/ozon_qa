2c26fab6a34a02606357b0260747c70ad46fa7ca
Marat Ramazanov <sadzax@gmail.com>
2023-01-27 18:34:46 +0300

    Solving

--- D_a.py
+++ D_a.py
@@ -1,29 +1,25 @@
 t = int(input())
-n = int(input())  # rows
-m = int(input())  # columns
-b = [[3, 4, 1], [2, 2, 5], [2, 4, 2], [2, 2, 1]]
-arr = b.copy()
-arr = []
-for _ in range(n):
-    row = list(map(int, input().split()))
-    arr.append(row)
-k = int(input())
-kc = list(map(int, input().split()))
+for iter in t:
+    n, m = list(map(int, input().split()))  # rows & columns
+    arr = []
+    for _ in range(n):
+        row = list(map(int, input().split()))
+        arr.append(row)
+    k = int(input())
+    kc = list(map(int, input().split()))
 
 
-def quicksort(array, x):
-    if len(array) < 2:
-        return array
-    else:
-        pivot = array[0][x-1]
-        less = [ls for ls in array[1:] if ls[x-1] < pivot]
-        greater = [ls for ls in array[1:] if ls[x-1] >= pivot]
-        return quicksort(less, x) + array[0] + quicksort(greater, x)
+    def quicksort(array, x):
+        if len(array) < 2:
+            return array
+        else:
+            pivot = array[0][x-1]
+            less = [ls for ls in array[1:] if ls[x-1] <= pivot]
+            greater = [ls for ls in array[1:] if ls[x-1] > pivot]
+            return quicksort(less, x) + [array[0]] + quicksort(greater, x)
 
 
-na = quicksort(arr, 3)
-
-
-for i in kc:
-    new_arr = quicksort(arr, i)
-
+    for i in kc:
+        res = quicksort(arr, i)
+    for i in res:
+        print(' '.join(map(str, i)))
